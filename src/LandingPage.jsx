88fce8902bfb0c9838a528bf3f24f02caa5317e4
tangentdigitalagency <david@tangentdigitalagency.com>
2021-10-04 17:45:16 -0700

    First Push

---
+++ src/LandingPage.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router';
+import './LandingPage.css';
+
+
+
+class LandingPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default withRouter(LandingPage)
